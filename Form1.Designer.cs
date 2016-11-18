34b6ac8e115aad6a9b3e03d035ea95c68bbda50a
AndriiTur <andriiturkevych@gmail.com>
2016-11-18 12:53:15 +0200

    fin
    finish

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace MethodSort
+{
+    partial class SortingForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.MethodSortButton = new System.Windows.Forms.Button();
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(12, 12);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(260, 20);
+            this.InputTextBox.TabIndex = 0;
+            // 
+            // MethodSortButton
+            // 
+            this.MethodSortButton.Location = new System.Drawing.Point(85, 102);
+            this.MethodSortButton.Name = "MethodSortButton";
+            this.MethodSortButton.Size = new System.Drawing.Size(113, 47);
+            this.MethodSortButton.TabIndex = 1;
+            this.MethodSortButton.Text = "MethodSort";
+            this.MethodSortButton.UseVisualStyleBackColor = true;
+            this.MethodSortButton.Click += new System.EventHandler(this.MethodSortButton_Click);
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(12, 229);
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.Size = new System.Drawing.Size(260, 20);
+            this.OutputTextBox.TabIndex = 2;
+            // 
+            // SortingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.MethodSortButton);
+            this.Controls.Add(this.InputTextBox);
+            this.Name = "SortingForm";
+            this.Text = "Sorting";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.Button MethodSortButton;
+        private System.Windows.Forms.TextBox OutputTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyLibrary;
+
+namespace MethodSort
+{
+    public partial class SortingForm : Form
+    {
+        public SortingForm()
+        {
+            InitializeComponent();
+        }
+        
+        private string ConvertStr(string str)
+        {
+            string[] arrStr = MyString.Split(str, " ");
+            int[] arrInt = new int[arrStr.Length];
+            for (int i = 0; i < arrStr.Length; i++)
+                arrInt[i] = Int32.Parse(arrStr[i]);
+            arrInt = ConvertArrInt(arrInt);
+            for (int i = 0; i < arrInt.Length; i++)
+                arrStr[i] = arrInt[i].ToString();
+            return MyString.Join(" ", arrStr);
+
+        }
+
+        private int[] ConvertArrInt(int[] arrInt)
+        {
+            int[] arrResult = new int[arrInt.Length];
+            int temp = arrInt[arrInt.Length-1];
+            int j = arrInt.Length - 2;
+            arrResult[arrResult.Length - 1] = arrInt[arrInt.Length - 1];
+            for (int i = arrInt.Length - 2; i >= 0; i--)
+            {
+                if (temp < arrInt[i])
+                {
+                    arrResult[i] = temp;
+                    temp = arrInt[i];
+                }
+                else
+                {
+                    arrResult[i] = temp;
+                }
+            }
+            return arrResult;
+        }
+
+        private void MethodSortButton_Click(object sender, EventArgs e)
+        {
+            OutputTextBox.Text = ConvertStr(InputTextBox.Text);
+        }
+
+       
+       
+    }
+}
